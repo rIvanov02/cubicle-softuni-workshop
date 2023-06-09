b5d3f86abb1df37e82fbce0eae1c58635dfda211
RobertIvanov02 <az_sum2324@abv.bg>
2023-06-10 02:15:13 +0300

    add search

--- src/managers/cubeManager.js
+++ src/managers/cubeManager.js
@@ -2,7 +2,21 @@ const uniqueID = require("uniqid");
 
 const cubes = [];
 
-exports.getAll = () => cubes.slice();
+exports.getAll = (search,from,to) => { 
+  const result = cubes.slice()
+
+  if (search) { 
+    result = result.filter(cube=>cube.name.toLowerCase().includes(search))
+  }
+
+  if (from) { 
+    result = result.filter(cube=>cube.difficultyLevel>=Number(from))
+  }
+  if (to) { 
+    result = result.filter(cube=>cube.difficultyLevel<=Number(to))
+  }
+  return result
+} 
 exports.getOne = (cubeId)=> cubes.find(x=> x.id == cubeId)
 exports.create = (cubeData) => {
   const newCube = {
