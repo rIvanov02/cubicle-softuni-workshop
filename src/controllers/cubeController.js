062bb335ef7a7bf4b972f0669e348dd688b3457a
RobertIvanov02 <az_sum2324@abv.bg>
2023-06-09 19:31:15 +0300

    add cube function

--- src/controllers/cubeController.js
+++ src/controllers/cubeController.js
@@ -1,13 +1,29 @@
 const router = require("express").Router();
-
+const cubeManager = require('../managers/cubeManager')
 router.get('/create', (req, res) => { 
     res.render('create')
 })
 
-router.post('/create', (req, res) => { 
-    console.log(req.body)
 
-    res.redirect("/")
-})
+    router.post('/create', (req, res) => { 
+        const { 
+            name,
+            description, 
+            imageUrl,
+            difficultyLevel,
+        } = req.body; 
+        
+        cubeManager.create({
+            name,
+            description,
+            imageUrl,
+            difficultyLevel: Number(difficultyLevel)
+        })
+        res.redirect("/")
+    })
+    
+
+
+
 
 module.exports = router
