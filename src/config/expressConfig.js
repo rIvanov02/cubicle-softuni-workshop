829a25f2e84290d3c272772d9bf9d276f7a44ee2
RobertIvanov02 <az_sum2324@abv.bg>
2023-06-09 17:47:51 +0300

    Add home controller

--- src/config/expressConfig.js
+++ src/config/expressConfig.js
@@ -1,9 +1,9 @@
-const path = require('path')
-const express = require('express');
+const path = require("path");
+const express = require("express");
 
-function expressConfig(app) { 
-    app.use(express.static(path.resolve(__dirname, "../public")))
-    app.use(express.urlencoded({extended: false}))
+function expressConfig(app) {
+  app.use(express.static(path.resolve(__dirname, "../public")));
+  app.use(express.urlencoded({ extended: false }));
 }
 
-module.exports = expressConfig
+module.exports = expressConfig;
--- src/index.js
+++ src/index.js
@@ -4,16 +4,15 @@ const app = express();
 
 const expressConfig = require('./config/expressConfig')
 const handlebarsConfig = require('./config/handlebarsConfig')
+const homeController = require('./controllers/homeController')
+
 
 expressConfig(app)
 handlebarsConfig(app)
 
 const port = 5000;
 
-// Routes
+app.use(homeController)
 
-app.get('/', (req, res) => { 
-    res.render("index")
-})
 
 app.listen(port, () => { console.log("Server listening on port : " + port)});
